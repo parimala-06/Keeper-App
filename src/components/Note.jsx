642e01d38df94946c2573961644e5dd380116b10
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-01-27 21:28:10 +0530

    Added Note

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Note() {
+    return(
+        <div className="note">
+            <h1>This Heading</h1>
+            <p>This is paragraph</p>
+        </div>
+    );
+}
+
+export default Note;
