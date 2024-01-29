e990f212a872b24372a59e1cbe6871122fa9a4ca
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-01-29 22:08:47 +0530

    Alterations in notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
 import CreateArea from "./CreateArea";
 
 function App(){
