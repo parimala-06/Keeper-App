3b0b595d1be4ad89ee125267e56213102f506dd9
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-01-29 22:03:28 +0530

    Added create template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App(){
     return(
         <div>
             <Header />
+            <CreateArea />
             {notes.map(noteItem => ( 
                 <Note 
                     key = {noteItem.key}
