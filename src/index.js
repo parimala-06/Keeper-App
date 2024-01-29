932cbf85b6de9d9b32d1eab1c453206dec6865cf
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-01-29 11:11:11 +0530

    Added notes.js and mapped noteItems

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom  from 'react-dom';
 import App from "./components/App"
 
 
-ReactDOM.render(<App />, document.getElementById('hi'));
+ReactDom.render(<App />, document.getElementById('hi'));
 
